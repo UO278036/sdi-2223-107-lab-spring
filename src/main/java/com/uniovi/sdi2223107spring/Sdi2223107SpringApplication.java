6cecf6ea95e9450772309ef9074d39057e89f15d
Raúl España <uo278036@uniovi.es>
2023-02-09 13:31:45 +0100

    SDI-2223-107-2.1-Create Spring project.

---
+++ src/main/java/com/uniovi/sdi2223107spring/Sdi2223107SpringApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi2223107spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi2223107SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sdi2223107SpringApplication.class, args);
+    }
+
+}
