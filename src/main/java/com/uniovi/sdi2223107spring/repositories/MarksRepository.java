cd352b94a5c47135824ebcc645c8b1cb937e2ddd
Raúl España <uo278036@uniovi.es>
2023-02-09 16:46:18 +0100

    SDI-2223-107-2.4-Modelo, acceso a datos simple.

---
+++ src/main/java/com/uniovi/sdi2223107spring/repositories/MarksRepository.java
@@ -0,0 +1,7 @@
+package com.uniovi.sdi2223107spring.repositories;
+
+import com.uniovi.sdi2223107spring.entities.Mark;
+import org.springframework.data.repository.CrudRepository;
+public interface MarksRepository extends CrudRepository<Mark, Long> {
+
+}
